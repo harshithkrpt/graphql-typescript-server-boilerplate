8af5bf11d27920b363632a678c96f7d7c8be8c56
harshithkrpt <harshith.krpt@gmail.com>
2020-03-30 21:03:47 +0530

    Basic Modular Template With Resolvers and Tests and Queries in one folder

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,13 +1,23 @@
 import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
 import * as path from "path";
+import * as fs from "fs";
+import { GraphQLSchema } from "graphql";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
 
-import { resolvers } from "./resolvers";
 import { createTypeormConn } from "./utils/create-typeorm-conn";
 
 export const startServer = async (): Promise<any> => {
-  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
 
   // Database Connection
   await createTypeormConn();
