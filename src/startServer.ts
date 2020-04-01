20e1a7842d9b5013f03711b571c4c88217436a89
harshithkrpt <harshith.krpt@gmail.com>
2020-04-01 21:51:36 +0530

    Twitter OAuth Code Removed

--- src/startServer.ts
+++ src/startServer.ts
@@ -5,15 +5,12 @@ import * as session from "express-session";
 import * as connectRedis from "connect-redis";
 import * as RateLimit from "express-rate-limit";
 import * as RateLimitRedisStore from "rate-limit-redis";
-import * as passport from "passport";
-import { Strategy } from "passport-twitter";
 
 import { redis } from "./redis";
 import { createTypeormConn } from "./utils/create-typeorm-conn";
 import { confirmEmail } from "./routes/confirmed-email";
 import { genSchema } from "./utils/generate-schema";
 import { redisSessionPrefix } from "./utils/constants";
-import { User } from "./entity/User";
 
 const RedisStore = connectRedis(session);
 
@@ -67,64 +64,7 @@ export const startServer = async () => {
 
   server.express.get("/confirm/:id", confirmEmail);
 
-  const connection = await createTypeormConn();
-
-  passport.use(
-    new Strategy(
-      {
-        consumerKey: process.env.TWITTER_CONSUME as string,
-        consumerSecret: process.env.TWITTER_CONSUME_SECRET as string,
-        callbackURL: "http://localhost:4000/auth/twitter/callback",
-        includeEmail: true
-      },
-      async (_, __, profile, cb) => {
-        const { id, emails } = profile;
-
-        const query = connection
-          .getRepository(User)
-          .createQueryBuilder("user")
-          .where("user.twitterId = :id", { id });
-
-        let email: string | null = null;
-        if (emails) {
-          email = emails[0].value;
-          query.orWhere("user.email = :email", { email });
-        }
-        let user = await query.getOne();
-
-        if (!user) {
-          // register
-          user = await User.create({
-            twitterId: id,
-            email
-          }).save();
-        } else if (!user.twitterId) {
-          // merge account
-          // we found user by email
-          user.twitterId = id;
-          await user.save();
-        } else {
-          // login
-        }
-
-        return cb(null, { id: user?.id });
-      }
-    )
-  );
-
-  server.express.use(passport.initialize());
-
-  server.express.get("/auth/twitter", passport.authenticate("twitter"));
-
-  server.express.get(
-    "/auth/twitter/callback",
-    passport.authenticate("twitter", { session: false }),
-    (req, res) => {
-      (req.session as any).userId = (req.user as any).id;
-      // TODO redirect to frontend
-      res.redirect("/");
-    }
-  );
+  await createTypeormConn();
 
   const app = await server.start({
     cors,
